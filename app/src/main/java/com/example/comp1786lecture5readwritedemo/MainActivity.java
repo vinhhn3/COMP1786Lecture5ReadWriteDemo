0c899606b99ecc69033e3f93026fef3368301097
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 14:55:42 +0700

    onResume

--- app/src/main/java/com/example/comp1786lecture5readwritedemo/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture5readwritedemo/MainActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String NOTES_FILE_NAME = "notes.txt";
@@ -35,6 +40,38 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG
             ).show();
         }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        try{
+            FileInputStream in = openFileInput(NOTES_FILE_NAME);
+            if (in != null){
+                InputStreamReader tmp = new InputStreamReader(in);
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuilder buf = new StringBuilder();
+                buf.append("Restored content: \n");
+
+                String str = reader.readLine();
+                while (str != null){
+                    buf.append(str + "\n");
+                    str = reader.readLine();
+                }
+
+                in.close();
+                EditText textEditor = findViewById(R.id.textEditor);
+                textEditor.setText(buf.toString());
+            }
+        }
+        catch (FileNotFoundException e){}
+        catch (Exception e){
+            Toast.makeText(this, "Exception " + e.toString(),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+
     }
 
 }
